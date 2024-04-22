ea9dd061bbf022ff5890f9a6dd309bbc93e8ed6f
Rifqi Naufal Tohari <rifqinaufaltohari@gmail.com>
2024-04-22 13:37:43 +0700

    remove spring cloud bus dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,6 @@ extra["springCloudVersion"] = "2023.0.0"
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.cloud:spring-cloud-config-server")
-	implementation("org.springframework.cloud:spring-cloud-starter-bus-amqp")
-	implementation("org.springframework.cloud:spring-cloud-config-monitor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
